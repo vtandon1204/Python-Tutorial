d3380d1ae30b473fd1529ecc8c1d4828d808369c
Vaibhav Tandon <vasu.tan2017@gmail.com>
2024-06-01 11:41:33 +0530

    commit

--- 3. intermediate python/4.filter_function.py
+++ 3. intermediate python/4.filter_function.py
@@ -5,6 +5,10 @@ def add7(x):
 def isOdd(x):
     return x%2!=0
 
-a = [1,2,3,4,5,6,8,9,10]
+a = [1,2,3,4,5,6,7,8,9,10]
 b = list(filter(isOdd,a))
-
+c = list(map(add7,b)) # using map function along with filter function
+# this 'c' list will take out the odd numbers (given by filter function in b)
+# and add 7 to them (given by map function)
+print(b)
+print(c)
