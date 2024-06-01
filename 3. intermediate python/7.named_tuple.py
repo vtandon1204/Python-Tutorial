a5c5f24a73bc68c600a5a31a3b377c371571df59
Vaibhav Tandon <vasu.tan2017@gmail.com>
2024-06-01 13:48:12 +0530

    commit

--- 3. intermediate python/7.named_tuple.py
+++ 3. intermediate python/7.named_tuple.py
@@ -1,4 +1,22 @@
-def func(x):
-    return x+5
+import collections
+from collections import namedtuple
 
+Point = namedtuple('Point','x y z')
+# Point is the name of object which is a namedtuple and 'x y z' are it's fields
 
+newP = Point(3,4,5) # consider Point as a Class
+print(newP)
+
+Point1 = namedtuple('Point_1', {'x':0, 'y':0, 'z':0})
+p1 = Point1(1,2,3)
+print(p1)
+print(p1.x,p1.y,p1.z)
+print(p1[0])
+print(newP._asdict())
+print(newP._fields)
+
+p2 = newP._replace(y=9)
+print(p2)
+
+p3 = Point._make(['a','b','c'])
+print(p3)
--- 3. intermediate python/8.deque.py
+++ 3. intermediate python/8.deque.py
@@ -1,1 +1,32 @@
+import collections
+from collections import deque
 
+d = deque("hello", maxlen=4)
+# d.maxlen = 7
+# attribute 'maxlen' of 'collections.deque' objects 
+# is not writable
+print(d)
+
+d.append(5)
+d.appendleft(10)
+print(d)
+
+d.pop()
+d.popleft()
+d.popleft()
+print(d)
+
+d.reverse()
+print(d)
+
+d.extend('2121')
+print(d)
+
+d.rotate(2)
+print(d)
+d.rotate(-3)
+print(d)
+
+
+d.clear()
+print(d) 
