8c03bc15494e906993fc89109edacc7f86ed055a
Vaibhav Tandon <vasu.tan2017@gmail.com>
2024-06-02 13:21:13 +0530

    commit

---
+++ 4. advanced python/2.dunder_magic_methods.py
@@ -0,0 +1,25 @@
+import inspect
+
+# print(inspect.getsource(list))
+x = [1,2, 3]
+y = [4,5]
+# print(x()3)
+print(x+y)
+print(x[1]) 
+print(type(x)) 
+
+
+class Person:
+    def __init__ (self,name):
+        self.name= name
+    def __repr__(self):
+        return f"{self.name}"
+    def __mul__(self,x):
+        if(type(x) is not int):
+            raise Exception("innvalid argument, must be int")
+        self.name = self.name*x
+p =Person('vt')
+p*4
+# print(p) # it shows it's memory address location
+print(p)
+
