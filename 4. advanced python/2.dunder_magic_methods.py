1130b4a64cf62fec02e004530c4f103f92cd52b8
Vaibhav Tandon <vasu.tan2017@gmail.com>
2024-06-02 13:53:37 +0530

    commit

--- 4. advanced python/2.dunder_magic_methods.py
+++ 4. advanced python/2.dunder_magic_methods.py
@@ -10,6 +10,7 @@ print(type(x))
 
 
 class Person:
+    
     def __init__ (self,name):
         self.name= name
     def __repr__(self):
@@ -19,7 +20,29 @@ class Person:
             raise Exception("innvalid argument, must be int")
         self.name = self.name*x
 p =Person('vt')
-p*4
-# print(p) # it shows it's memory address location
+# p*4
+# print(p) # it shows it's memory address location of the object 
 print(p)
 
+
+# print(dir(int)) # it lists all the dunder functions of integer
+
+a= 100
+a.__add__(5)
+print(a)
+
+
+class vt:
+    # def __new__(cls): # helps in term of creating a new instance
+    #     print("this is new")
+        
+    def __init__(self):
+        print("this is init")
+        self.phone=788
+    
+    def __str__(self):
+        return "this is magic call of str"
+    
+v = vt()
+print(v)
+print(v.phone)
--- 4. advanced python/tempCodeRunnerFile.py
+++ 4. advanced python/tempCodeRunnerFile.py
@@ -1,1 +1,14 @@
 
+class vt:
+    def __new__(cls): # helps in term of creating a new instance
+        print("this is new")
+        
+    def __init__(self):
+        print("this is init")
+        self.phone=788
+    
+    def __str__(self):
+        return "this is magic call of str"
+    
+v = vt()
+print(v)
