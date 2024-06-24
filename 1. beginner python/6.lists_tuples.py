72b5998af4d455a82d079396d77c6df991aa0e0d
Vaibhav Tandon <vasu.tan2017@gmail.com>
2024-06-24 14:21:11 +0530

    commit

--- 1. beginner python/6.lists_tuples.py
+++ 1. beginner python/6.lists_tuples.py
@@ -1,19 +1,19 @@
-# # LISTS: collection of same/different datatypes
-# fruits = ['apple', 'pear', 3]
-# print(fruits)
+# LISTS: collection of same/different datatypes
+fruits = ['apple', 'pear', 3]
+print(fruits)
 
-# # printing single element of list
-# print(fruits[1])
-# for i in range(0,3):
-#     print(fruits[i])
+# printing single element of list
+print(fruits[1])
+for i in range(0,3):
+    print(fruits[i])
 
-# # add an element to the list
-# fruits.append('banana') # append() will add the element to the end of the list
-# print(fruits)
+# add an element to the list
+fruits.append('banana') # append() will add the element to the end of the list
+print(fruits)
 
-# # change the existing element of the list
-# fruits[1] = 'orange'
-# print(fruits)
+# change the existing element of the list
+fruits[1] = 'orange'
+print(fruits)
 
 # TUPLES: used for coordinates
 position = (2,3)
