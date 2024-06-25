14017846f4503ca2290dc4c773316c4f4e75369d
Vaibhav Tandon <vasu.tan2017@gmail.com>
2024-06-25 13:31:08 +0530

    commmit

--- 1. beginner python/14.modular_prog.py
+++ 1. beginner python/14.modular_prog.py
@@ -3,9 +3,11 @@
 import math
 # import pygame # for game dev by python
 import os # for file pass, import images, import transitive things/files in python
-import myModule
 print(math.sqrt(144))
 print(math.pi)
 print(math.degrees(math.pi))
+
+
+import myModule
 print(myModule.myFunc(5))
 print(myModule.Func2(500))
