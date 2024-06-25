bf7ce1e7c0b185f78f9c228172dbc0be961a68cd
Vaibhav Tandon <vasu.tan2017@gmail.com>
2024-06-25 12:50:52 +0530

    commit

--- 1. beginner python/10.functions.py
+++ 1. beginner python/10.functions.py
@@ -10,3 +10,18 @@ def accleration(mass,force):
 print(addTwo(4))
 print(substractTwo(5))
 print(accleration(4,2))
+
+def calc_total(exp):
+    total = 0
+    for item in exp:
+        total += item
+    return total
+
+vt_exp = [2100,200,2200]
+sd_exp = [400,2400,700]
+
+vt_total = calc_total(vt_exp)
+sd_total = calc_total(sd_exp)
+
+print("vt total expenses are",vt_total)
+print("sd total expenses are",sd_total)
